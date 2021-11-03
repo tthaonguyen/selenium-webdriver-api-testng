1746a34067662f84aee7777ae658ad761c6906ff
Yuki <Yuki@Matchi>
2021-11-03 15:21:02 +0700

    Topic 02 Xpath and css

--- src/webDriver/Topic_03_Run_On_FireFox_Chrome.java
+++ src/webDriver/Topic_03_Run_On_FireFox_Chrome.java
@@ -7,12 +7,17 @@ import org.testng.annotations.Test;
 
 //Lesson: set up environment
 
-public class Topic_01_FireFox_Chrome {
+public class Topic_03_Run_On_FireFox_Chrome {
 	WebDriver driver;
+	String project_location = System.getProperty("user.dir");
 
 	@Test
 	public void TC_01_RunOnChrome() {
-		System.setProperty("webdriver.chrome.driver", ".\\browserDriver\\chromedriver.exe");
+		//relative path
+		//Sol 1:
+		//System.setProperty("webdriver.chrome.driver", ".\\browserDriver\\chromedriver.exe");
+		//Sol 2:
+		System.setProperty("webdriver.chrome.driver", project_location + "\\browserDriver\\chromedriver.exe");
 
 		driver = new ChromeDriver();
 
---
+++ src/webDriver/Topic_04_Exercise_Command.java
@@ -0,0 +1,9 @@
+package webDriver;
+
+import org.testng.annotations.Test;
+
+public class Topic_04_Exercise_Command {
+  @Test
+  public void f() {
+  }
+}
