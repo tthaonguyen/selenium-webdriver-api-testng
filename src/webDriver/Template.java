b198e90df926e48c77ba384260685dbd1501255a
tthaonguyen <thaonguyen.project@gmail.com>
2021-12-16 16:59:55 +0700

    Topic_15_Handle_Custom_DropdownList_Part_I.java

---
+++ src/webDriver/Template.java
@@ -0,0 +1,36 @@
+package webDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+
+public class Template {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+	}
+
+	@Test
+	public void TC_01() {
+		
+	}
+	 
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
